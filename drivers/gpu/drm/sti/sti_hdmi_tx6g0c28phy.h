b4770e9ec90f637afd6d81850deed9e4bf59ee96
Alain Volmat <avolmat@me.com>
2023-07-27 21:51:25 +0000

    drm/sti: add hdmi tx6g0c28 phy for STi platform
    Addition of the HDMI TX PHY driver for use in the STiH418
    SoC platform and more especially the 4KOpen (B2264) board.
    
    Signed-off-by: Alain Volmat <avolmat@me.com>

---
+++ drivers/gpu/drm/sti/sti_hdmi_tx6g0c28phy.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Author: Alain Volmat <avolmat@me.com>
+ */
+
+#ifndef _STI_HDMI_TX6G0C28PHY_H_
+#define _STI_HDMI_TX6G0C28PHY_H_
+
+#include "sti_hdmi.h"
+
+extern struct hdmi_phy_ops tx6g0c28phy_ops;
+
+#endif
