cb66f10e707b3c19e9f0b0932feea983eb91bdb0
Jerome Audu <jau@free.fr>
2023-08-23 14:58:20 +0200

    pinctrl: st: use dynamic allocation of GPIO base
    Since commit 502df79b860563d7 ("gpiolib: Warn on drivers still using static
    gpiobase allocation"), one or more warnings are printed during boot on
    systems where static allocation of GPIO base is used:
    
    [    0.022795] gpio gpiochip0: Static allocation of GPIO base is deprecated, use dynamic allocation.
    [    0.023098] st-pinctrl 961f080.pin-controller-sbc: PIO0 bank added.
    [    0.023174] gpio gpiochip1: Static allocation of GPIO base is deprecated, use dynamic allocation.
    [    0.023375] st-pinctrl 961f080.pin-controller-sbc: PIO1 bank added.
    [    0.023447] gpio gpiochip2: Static allocation of GPIO base is deprecated, use dynamic allocation.
    [    0.023637] st-pinctrl 961f080.pin-controller-sbc: PIO2 bank added.
    
    So let's follow the suggestion and use dynamic allocation.

--- drivers/pinctrl/pinctrl-st.c
+++ drivers/pinctrl/pinctrl-st.c
@@ -1506,7 +1506,7 @@ static int st_gpiolib_register_bank(struct st_pinctrl *info,
 		return PTR_ERR(bank->base);
 
 	bank->gpio_chip = st_gpio_template;
-	bank->gpio_chip.base = bank_num * ST_GPIO_PINS_PER_BANK;
+	bank->gpio_chip.base = -1;
 	bank->gpio_chip.ngpio = ST_GPIO_PINS_PER_BANK;
 	bank->gpio_chip.fwnode = of_fwnode_handle(np);
 	bank->gpio_chip.parent = dev;
@@ -1516,7 +1516,7 @@ static int st_gpiolib_register_bank(struct st_pinctrl *info,
 	bank->gpio_chip.label = range->name;
 
 	range->id = bank_num;
-	range->pin_base = range->base = range->id * ST_GPIO_PINS_PER_BANK;
+	range->pin_base = range->base = range->id * ST_GPIO_PINS_PER_BANK + 512;
 	range->npins = bank->gpio_chip.ngpio;
 	range->gc = &bank->gpio_chip;
 
